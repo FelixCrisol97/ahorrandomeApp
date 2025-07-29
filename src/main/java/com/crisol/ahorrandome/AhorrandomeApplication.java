cd956733b8237b7f133cecc108a67655d6ad1730
Felix Lopez Leobardo <felix.lopez.leobardo.social@outlook.com>
2025-07-28 18:58:07 -0600

    :tada: Inicio de proyecto

---
+++ src/main/java/com/crisol/ahorrandome/AhorrandomeApplication.java
@@ -0,0 +1,13 @@
+package com.crisol.ahorrandome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhorrandomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhorrandomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crisol/ahorrandome/AhorrandomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisol.ahorrandome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhorrandomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
